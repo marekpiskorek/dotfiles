0674c8b9fa175e11c978397aa4cb11dfc3739eef
Marek Piskorek <marek.piskorek@gmail.com>
2021-07-28 21:50:57 +0200

    How cool is to run nvim with just typing v<CR>?

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 # ALIASES
 alias k=kubectl
 alias g=git
-alias vim=~/bin/appimages/nvim.appimage
+alias v=~/bin/appimages/nvim.appimage
 alias ls="lsd -la"
 alias run-codility="~/workspace/codility/run django:port=8001 backend-worker webpack-dev-server task-server checker docker_server"
 
