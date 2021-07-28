1efffc749ce02930f3259181678a77d9dda4e8f8
Marek Piskorek <marek.piskorek@gmail.com>
2021-07-28 21:49:33 +0200

    Remove Regolith setup altogether. Add autoload on TMUX and rough install file.

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,11 @@ alias vim=~/bin/appimages/nvim.appimage
 alias ls="lsd -la"
 alias run-codility="~/workspace/codility/run django:port=8001 backend-worker webpack-dev-server task-server checker docker_server"
 
+# Add GOPATH
 set -x -U GOPATH $HOME/go
 set PATH $GOPATH/bin $PATH
+
+# Run tmux on shell start
+if ! test $TERM = screen-256color
+  exec tmux new-session -A -s main 
+end
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# nvim
+ln -s ${BASEDIR}/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ${BASEDIR}/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+
+# git
+ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+
+# tmux
+ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
