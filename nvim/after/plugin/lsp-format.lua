5c5285247aec6cd55d8a7d35e68fefb531118322
Marek Piskorek <marek.piskorek@pm.me>
2022-08-03 15:41:56 +0200

    Add a comment for lsp-format

--- nvim/after/plugin/lsp-format.lua
+++ nvim/after/plugin/lsp-format.lua
@@ -1,4 +1,6 @@
 require("lsp-format").setup {}
+-- Every LSP server we want to have autoformatting for needs to be listed here.
+-- For sanity purposes this list is in the same order as the one in lua/marek/lsp.lua
 require "lspconfig".tsserver.setup { on_attach = require "lsp-format".on_attach }
 require "lspconfig".dockerls.setup { on_attach = require "lsp-format".on_attach }
 require "lspconfig".gopls.setup { on_attach = require "lsp-format".on_attach }
