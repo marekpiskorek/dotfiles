861c9e9d1f972490aaea5f1275648d80c37c49ff
Marek Piskorek <marek.piskorek@pm.me>
2022-08-03 17:20:03 +0200

    Update the install script

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/bash
 
+# TODO: install the dependencies
+
+# Prepare symlinks for configs
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# nvim
-ln -s ${BASEDIR}/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ${BASEDIR}/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-
-# git
 ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
-
-# tmux
-ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
-
-# i3
-ln -s ${BASEDIR}/i3/config ~/.config/i3/config
+rm -f ~/.zshrc  # Removing the .zshrc so that the script won't break on symlink creation
+ln -s ${BASEDIR}/.zshrc ~/.zshrc
+ln -s ${BASEDIR}/tmux ~/.config/tmux
+ln -s ${BASEDIR}/nvim ~/.config/nvim
+ln -s ${BASEDIR}/alacritty ~/.config/alacritty
