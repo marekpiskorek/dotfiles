d626d095cff598d957add855b7853aeb3071beb6
Marek Piskorek <marek.piskorek@samsara.com>
2024-02-04 20:33:38 +0100

    Added two scripts: one for lf preview of image files, another for limitting my mGBA playtime.

---
+++ bin/mgba.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import subprocess
+
+command = "mGBA-0.10.3-appimage-x64.appimage --appimage-extract-and-run &"
+text = "I have nothing better to do. I will waste time."
+
+print(
+    "You are about to run a mGBA emulator.\n"
+    "You are going to play games instead of doing something valuable,\n"
+    "like work, learning or improving your environment.\n"
+)
+
+typed = input("If you are sure, type those exact words: " + text + "\n")
+
+if typed == text:
+    subprocess.check_call(command, shell=True, executable="/bin/bash")
+else:
+    print("BOOO! Go back to work!")
