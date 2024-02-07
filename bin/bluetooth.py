0230798d75d3693d4ed17dc90aa7ec39b1ebe792
Marek Piskorek <marek.piskorek@samsara.com>
2024-02-07 20:27:39 +0100

    Use asyncio task groups in bluetooth connection script

--- bin/bluetooth.py
+++ bin/bluetooth.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import argparse
+from typing import Optional
+import asyncio
 
 ALL = "all"
 TUXEDO = "tuxedo"
@@ -25,37 +27,39 @@ def init_argparse() -> argparse.ArgumentParser:
     return parser
 
 
-def call_bash(command: str) -> None:
+async def call_bash(command: str) -> Optional[bytes]:
     try:
-        output = subprocess.check_output(
-            command, shell=True, executable="/bin/bash")
+        return subprocess.check_output(command, shell=True, executable="/bin/bash")
     except subprocess.CalledProcessError as e:
         print("ERROR", e)
         exit()
-    else:
-        if output:
-            print(output.decode("utf-8"))
 
 
-def main() -> None:
+async def connect(device: str, address: str):
+    print(f"Connecting to {device}: {address}")
+    await call_bash(f"bluetoothctl connect {address}")
+
+
+async def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
 
-    call_bash("pacmd load-module module-bluetooth-policy")
-    call_bash("pacmd load-module module-bluetooth-discover")
-
-    call_bash("bluetoothctl power on")
+    # First asyncio task group - module loading and bluetooth preparation
+    async with asyncio.TaskGroup() as tg:
+        print("Loading bluetooth modules")
+        tg.create_task(call_bash("pacmd load-module module-bluetooth-policy"))
+        tg.create_task(call_bash("pacmd load-module module-bluetooth-discover"))
+        tg.create_task(call_bash("bluetoothctl power on"))
 
     tuxedo_address = "4C:87:5D:CB:5E:08 "
     mouse_address = "D7:F7:4C:10:BD:77"
 
-    if args.target in (ALL, TUXEDO):
-        print(f"Connecting to Tuxedo: {tuxedo_address}")
-        call_bash(f"bluetoothctl connect {tuxedo_address}")
+    # Second asyncio group - connection to particular devices
+    async with asyncio.TaskGroup() as tg:
+        if args.target in (ALL, TUXEDO):
+            tg.create_task(connect(TUXEDO, tuxedo_address))
 
-    if args.target in (ALL, MOUSE):
-        print(f"Connecting to mouse: {mouse_address}")
-        call_bash(f"bluetoothctl connect {mouse_address}")
+        if args.target in (ALL, MOUSE):
+            tg.create_task(connect(MOUSE, mouse_address))
 
-
-main()
+asyncio.run(main())
